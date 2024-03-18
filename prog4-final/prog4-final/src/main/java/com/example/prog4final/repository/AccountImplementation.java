1e93ca4db6a32c06f536855fb1a53fcfdb661c24
lovasoa3 <hei.lovasoa.3@gmail.com>
2024-03-18 07:33:58 +0100

    feat: nouvelle fonctionnaliter qui consiste a afficher tout les comptes

--- prog4-final/prog4-final/src/main/java/com/example/prog4final/repository/AccountImplementation.java
+++ prog4-final/prog4-final/src/main/java/com/example/prog4final/repository/AccountImplementation.java
@@ -24,7 +24,23 @@ public class AccountImplementation implements AccountDAO {
 
     @Override
     public List<Account> findAll() throws SQLException {
-    return null;
+        List<Account> allAccount = new ArrayList<>();
+        String sql ="SELECT * FROM account";
+        try(PreparedStatement preparedStatement= connection.prepareStatement(sql)){
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()){
+                allAccount.add(new Account(
+                        resultSet.getInt("id_account"),
+                        resultSet.getString("lastName"),
+                        resultSet.getString("firstName"),
+                        resultSet.getDate("birthDate"),
+                        resultSet.getDouble("salary"),
+                        resultSet.getString("accountNumber")
+
+                ));
+            }
+        }
+        return allAccount;
     }
 
 
